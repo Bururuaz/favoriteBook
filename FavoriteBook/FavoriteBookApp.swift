c76b4477a9336dec0240487e33c0315db2a8fa5b
Emrecan <emrecanaslan511@gmail.com>
2024-05-05 16:58:02 +0300

    Initial Commit

---
+++ FavoriteBook/FavoriteBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteBookApp.swift
+//  FavoriteBook
+//
+//  Created by APPLE on 5.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct FavoriteBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
